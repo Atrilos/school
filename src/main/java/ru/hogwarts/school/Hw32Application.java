6f8685733b465c74e818914451d550f3a1c46787
Atrilos <101889118+Atrilos@users.noreply.github.com>
2022-10-20 01:39:06 +0700

    init

---
+++ src/main/java/ru/hogwarts/school/Hw32Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw32Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hogwarts/school/Hw32ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
