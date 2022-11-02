e5c279aa426ee4897e030dd558065daa6bc33c67
Atrilos <101889118+Atrilos@users.noreply.github.com>
2022-11-02 23:27:31 +0700

    HW3-6 complete
    -Create WebMvcTest for FacultyController
    -Create broad integration test for StudentController using Testcontainer

---
+++ src/test/java/ru/hogwarts/school/controller/constants/FacultyControllerTestConstants.java
@@ -0,0 +1,9 @@
+package ru.hogwarts.school.controller.constants;
+
+import ru.hogwarts.school.model.Faculty;
+
+public class FacultyControllerTestConstants {
+    public static final Faculty FACULTY_1 = new Faculty(2L, "blU", "Blue", null);
+    public static final Faculty FACULTY_2 = new Faculty(3L, "Blu", "Blue", null);
+    public static final Faculty FACULTY_3 = new Faculty(4L, "Ion", "Blue", null);
+}
