79694f1ecf0f248f4af98ca2c2d11df1d21dc1d2
Atrilos <101889118+Atrilos@users.noreply.github.com>
2022-11-07 02:48:12 +0700

    Change MapStruct to ModelMapper
    -ModelMapper instead of MapStruct because of bad api of latter

---
+++ src/main/java/ru/hogwarts/school/mapper/MapperConfiguration.java
@@ -0,0 +1,14 @@
+package ru.hogwarts.school.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -3,8 +3,7 @@ package ru.hogwarts.school.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exceptions.EntryNotFoundException;
-import ru.hogwarts.school.mapper.FacultyMapper;
-import ru.hogwarts.school.mapper.StudentMapper;
+import ru.hogwarts.school.mapper.Mapper;
 import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -22,12 +21,11 @@ public class StudentService {
     private final FacultyService facultyService;
     private final AvatarService avatarService;
     private final StudentRepository studentRepository;
-    private final StudentMapper studentMapper;
-    private final FacultyMapper facultyMapper;
+    private final Mapper mapper;
 
     public StudentDto addStudent(NewStudentDto student) {
-        Student studentToAdd = studentMapper.toEntity(student);
-        return studentMapper.toDto(studentRepository.save(studentToAdd));
+        Student studentToAdd = mapper.toEntity(student);
+        return mapper.toDto(studentRepository.save(studentToAdd));
     }
 
     public void removeStudent(Long id) {
@@ -37,19 +35,26 @@ public class StudentService {
 
     public StudentDto updateStudent(long id, StudentDto student) {
         Student foundStudent = getStudentById(id);
-        Faculty faculty = facultyService.getFacultyById(student.getFaculty().getId());
-        Avatar avatar = avatarService.getAvatarById(student.getAvatar().getId());
+
+        Faculty faculty = null;
+        if (student.getFaculty() != null)
+            faculty = facultyService.getFacultyById(student.getFaculty().getId());
+
+        Avatar avatar = null;
+        if (student.getAvatar() != null)
+            avatar = avatarService.getAvatarById(student.getAvatar().getId());
+
         foundStudent.setName(student.getName());
         foundStudent.setFaculty(faculty);
         foundStudent.setAvatar(avatar);
-        return studentMapper.toDto(studentRepository.save(foundStudent));
+        return mapper.toDto(studentRepository.save(foundStudent));
     }
 
     public Collection<StudentDto> getStudentsByAge(int age) {
         return studentRepository
                 .findStudentsByAge(age)
                 .stream()
-                .map(studentMapper::toDto)
+                .map(mapper::toDto)
                 .toList();
     }
 
@@ -60,30 +65,30 @@ public class StudentService {
         return studentRepository
                 .findStudentsByAgeBetween(from, to)
                 .stream()
-                .map(studentMapper::toDto)
+                .map(mapper::toDto)
                 .toList();
     }
 
     public FacultyDto getFacultyByStudent(Long studentId) {
         Faculty faculty = studentRepository.findStudentsFaculty(studentId);
-        return facultyMapper.toDto(faculty);
+        return mapper.toDto(faculty);
     }
 
     public StudentDto getStudent(long id) {
         Student foundStudent = getStudentById(id);
-        return studentMapper.toDto(foundStudent);
-    }
-
-    protected Student getStudentById(Long id) {
-        return studentRepository
-                .findById(id)
-                .orElseThrow(() -> new EntryNotFoundException("The specified student not found"));
+        return mapper.toDto(foundStudent);
     }
 
     public StudentDto patchStudentAvatar(long id, long avatarId) {
         Student student = getStudentById(id);
         Avatar avatar = avatarService.getAvatarById(avatarId);
         student.setAvatar(avatar);
-        return studentMapper.toDto(studentRepository.save(student));
+        return mapper.toDto(studentRepository.save(student));
+    }
+
+    protected Student getStudentById(Long id) {
+        return studentRepository
+                .findById(id)
+                .orElseThrow(() -> new EntryNotFoundException("The specified student not found"));
     }
 }
